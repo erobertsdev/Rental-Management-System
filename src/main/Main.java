b47271f535837f0a1d9e24836c1e802fc31973e5
Elias Roberts <elias@eroberts.dev>
2022-05-06 13:49:11 -0600

    bare bones login screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource(fxml_file));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("..\\view\\loginForm.fxml")));
         stage.setTitle("Login Screen");
         stage.setScene(new Scene(root));
         stage.show();
