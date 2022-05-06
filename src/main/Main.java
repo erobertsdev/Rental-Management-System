655328c6861db89ac3c99164087020fcba970cbe
Elias Roberts <elias@eroberts.dev>
2022-05-06 12:24:58 -0600

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
