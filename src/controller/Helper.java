25e35bd76ec17b271f7e8229a9472301814b0003
Elias Roberts <elias@eroberts.dev>
2022-05-09 10:54:49 -0600

    added a few locale functions

---
+++ src/controller/Helper.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.util.Locale;
+import java.util.Objects;
+
+/** Class with helper functions */
+public class Helper {
+
+    /** Set Locale
+     * @return The locale of the system */
+    private static Locale setLocale() {
+        Locale locale = Locale.getDefault();
+        if (Objects.equals(locale.getLanguage(), "fr")) {
+            locale = new Locale("fr", "CA");
+        }
+        locale = new Locale("en", "US");
+        return locale;
+    }
+
+    /** Get system language
+     * @return system language */
+    public String getLanguage() {
+        Locale locale = Locale.getDefault();
+        return locale.getLanguage();
+    }
+
+    /** Returns system country
+     * @return system country */
+    public String getCountry() {
+        Locale locale = Locale.getDefault();
+        return locale.getDisplayCountry();
+    }
+
+    /** Returns results from setLocale()
+     * @return system locale */
+    public static Locale getLocale() {
+        return setLocale();
+    }
+}
---
+++ src/controller/LoginForm.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginForm implements Initializable {
+    @FXML
+    private Label zoneLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        zoneLabel.setText(Helper.getLocale().toString());
+    }
+}
