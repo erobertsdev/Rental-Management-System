b63470712f6afe38fe65272490f287142e73b883
Elias Roberts <elias@eroberts.dev>
2022-05-28 13:49:00 -0600

    hours and minutes populate in appointmentForm

--- src/controller/AppointmentForm.java
+++ src/controller/AppointmentForm.java
@@ -1,5 +1,7 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -14,59 +16,36 @@ import java.util.ResourceBundle;
 
 public class AppointmentForm implements Initializable {
 
-    @FXML
-    private TextField appointmentDescriptionTextField;
-
-    @FXML
-    private TextField appointmentIdTextField;
-
-    @FXML
-    private TextField appointmentLocationTextField;
-
-    @FXML
-    private TextField appointmentTitleTextField;
-
-    @FXML
-    private TextField appointmentTypeTextField;
-
-    @FXML
-    private Button cancelButton;
-
-    @FXML
-    private ComboBox<?> contactCombo;
-
-    @FXML
-    private ComboBox<?> customerCombo;
-
-    @FXML
-    private AnchorPane endDate;
-
-    @FXML
-    private DatePicker endDatePicker;
-
-    @FXML
-    private ChoiceBox<?> endHourChoice;
-
-    @FXML
-    private ChoiceBox<?> endMinuteChoice;
-
-    @FXML
-    private Button saveButton;
-
-    @FXML
-    private DatePicker startDatePicker;
-
-    @FXML
-    private ChoiceBox<?> startHourChoice;
-
-    @FXML
-    private ChoiceBox<?> startMinuteChoice;
-
-    @FXML
-    private ComboBox<?> userCombo;
+    @FXML private TextField appointmentDescriptionTextField;
+    @FXML private TextField appointmentIdTextField;
+    @FXML private TextField appointmentLocationTextField;
+    @FXML private TextField appointmentTitleTextField;
+    @FXML private TextField appointmentTypeTextField;
+    @FXML private Button cancelButton;
+    @FXML private ComboBox<?> contactCombo;
+    @FXML private ComboBox<?> customerCombo;
+    @FXML private AnchorPane endDate;
+    @FXML private DatePicker endDatePicker;
+    @FXML private ChoiceBox<String> endHourChoice;
+    @FXML private ChoiceBox<String> endMinuteChoice;
+    @FXML private Button saveButton;
+    @FXML private DatePicker startDatePicker;
+    @FXML private ChoiceBox<String> startHourChoice;
+    @FXML private ChoiceBox<String> startMinuteChoice;
+    @FXML private ComboBox<?> userCombo;
+    private final ObservableList<String> hours = FXCollections.observableArrayList();
+    private final ObservableList<String> minutes = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Hour and minute choicebox values
+        hours.addAll("0","1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12",
+                "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+        minutes.addAll("0", "15", "30", "45");
+        startHourChoice.setItems(hours);
+        endHourChoice.setItems(hours);
+        startMinuteChoice.setItems(minutes);
+        endMinuteChoice.setItems(minutes);
 
     }
 }
