21b73381a377296e4a0ed6a8a869cb3e37dbf32d
Elias Roberts <elias@eroberts.dev>
2022-06-24 18:20:48 -0600

    empty tests

---
+++ src/controller/CustomerFormTest.java
@@ -0,0 +1,55 @@
+package controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerFormTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForAppointments() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleSearchButton() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleSellButton() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleEditCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleAddCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleDeleteCustomer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleEditAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleAddAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleDeleteAppointment() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void handleMyAppointments() {
+    }
+}
